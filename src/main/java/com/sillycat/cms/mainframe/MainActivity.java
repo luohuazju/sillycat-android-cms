933927e21eb91fd3094b991adfd5ea72740abdc6
Carl <luohuazju@gmail.com>
2014-06-14 01:56:42 -0500

    init of the project

---
+++ src/main/java/com/sillycat/cms/mainframe/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sillycat.cms.mainframe;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class MainActivity extends Activity {
+
+	protected static final String TAG = MainActivity.class.getSimpleName();
+
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+	}
+
+}
